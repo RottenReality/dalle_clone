c468c29abdb1c1534db3d5b936e5b147982874c6
Andres Orrego <andresorregolopez@gmail.com>
2025-04-07 11:42:11 -0500

    feat: create creat-post page structure

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,9 +6,61 @@ import { getRandomPrompt } from '../utils'
 import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  })
+  const [generatingImg, setGeneratingImg] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+  
   return (
 
-    <div>CreatePost</div>
+    <section className='max-2-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'>Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
+
+        <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-5'>
+            <FormField
+              labelName="Your name"
+              type="text"
+              name="name"
+              placeholder="Jhon Doe"
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField
+              labelName="prompt"
+              type="text"
+              name="prompt"
+              placeholder="A comic book cover of a superhero wearing headphones"
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+          </div>
+        </form>
+      </div> 
+    </section>
   )
 }
 
