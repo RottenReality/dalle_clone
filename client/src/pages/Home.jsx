21b325700973cbb78c282154b7e516d063a22f97
Andres Orrego <andresorregolopez@gmail.com>
2025-05-03 16:53:49 -0500

    feat: add cloudinary and post image functions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,31 @@ const Home = () => {
 
     const [searchText, setSearchText] = useState("")
 
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true)
+
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/post', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+
+                if(response.ok) {
+                    const result = await response.json()
+
+                    setAllPosts(result.data.reverse())
+                }
+            } catch (error) {
+                alert(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+    })
+
     const RenderCards = ({ data, title }) => {
         if(data?.length > 0) return data.map((post) => <Card key= {post._id} {...post}/>)
         
@@ -48,7 +73,7 @@ const Home = () => {
                                 />
                             ) : (
                                 <RenderCards
-                                    data={[]}
+                                    data={allPosts}
                                     title="No posts found"
                                 />
                             )}
